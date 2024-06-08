8596f2335e94de5996e0c5be67ff5d21457c0623
sofia-verawati <verawatisovia@gmail.com>
2024-06-07 23:02:36 -0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, ts, jsx, tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'background': '#29241e',
+        'text': '#f7f9fa',
+        'title': '#4cb6f7',
+        'title-hover': '#5ca8ff',
+        'bg': '#1d2120',
+      },
+    },
   },
   plugins: [],
 }
